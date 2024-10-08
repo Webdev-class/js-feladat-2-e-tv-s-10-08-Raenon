7323e447925b86e4c1af497874d59d43b129af8c
rae <krstfboda@gmail.com>
2024-10-08 13:19:09 +0200

    kesz a feladat :)

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,14 @@
 
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 async function f1 (lat, long, timeStr) {
-    //TODO
+    const queryStr = APIAdress + `latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
+    const rArr =  await fetch(queryStr).then(data => data.json());
+    let sum = 0;
+    rArr.hourly.temperature_2m.forEach(element => {
+        sum += element;
+    });
+    return Math.floor(sum / 24);
+
 }
 
 module.exports = f1;
--- f2/f2.js
+++ f2/f2.js
@@ -1,6 +1,9 @@
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 
 const f2 = async (lat, long, timeStr, hour) => {
-    //TODO
+    const queryStr = APIAdress + `latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
+    const rArr =  await fetch(queryStr).then(data => data.json());
+
+    return rArr.hourly.temperature_2m.at(hour);
 }
 module.exports = f2;
